0e1d0bd0187c025e9e34553ef09dc5d91af78086
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2025-10-28 01:21:10 +0300

    snap

---
+++ source/scripts/update.sh
@@ -0,0 +1,16 @@
+BRANCH=v2.1.3
+
+function get_pocketpy() {
+  cd source/thirdparty && \
+  git clone -b ${BRANCH} --depth=1 https://github.com/pocketpy/pocketpy.git && \
+  rm -rf pocketpy/.git
+}
+
+if [[ -d "source/thirdparty/pocketpy" ]]; then
+  echo "Directory exists."
+  mv source/thirdparty/pocketpy source/thirdparty/pocketpy.old
+  get_pocketpy
+else
+  echo "Directory does not exist."
+  get_pocketpy
+fi
